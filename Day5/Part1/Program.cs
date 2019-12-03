99a6800e23bb01c3327a326e5764757a1c525913
Dominik Szymański <domints@gmail.com>
2019-12-03 01:51:35 +0100

    Add day 5

---
+++ Day5/Part1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bootUp");
+            var start = File.ReadAllText("input.txt").Trim();
+            LinkedList<char> polymer = new LinkedList<char>(start);
+            while(CleanUp(polymer)) ;
+            Console.WriteLine(polymer.Count);
+        }
+
+        static bool CleanUp(LinkedList<char> input)
+        {
+            bool changed = false;
+            LinkedListNode<char> lastNode = input.First;
+            while(lastNode != null && lastNode.Next != null)
+            {
+                if(ShouldRemove(lastNode.Value, lastNode.Next.Value))
+                {
+                    changed = true;
+                    var newLastNode = lastNode.Next.Next;
+                    input.Remove(lastNode.Next);
+                    input.Remove(lastNode);
+                    lastNode = newLastNode;
+                }
+                else
+                {
+                    lastNode = lastNode.Next;
+                }
+            }
+
+            return changed;
+        }
+
+        static bool ShouldRemove(char current, char next)
+        {
+            return (char.IsUpper(current) && char.IsLower(next) && current == char.ToUpper(next))
+                || (char.IsUpper(next) && char.IsLower(current) && next == char.ToUpper(current));
+        }
+    }
+}
---
+++ Day5/Part2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bootUp");
+            var lowestValue = int.MaxValue;
+            for(char i = 'a'; i <= 'z'; i++)
+            {
+                var start = File.ReadAllText("input.txt")
+                    .Trim()
+                    .Replace(i.ToString(), "")
+                    .Replace(char.ToUpper(i).ToString(), "");
+                LinkedList<char> polymer = new LinkedList<char>(start);
+                while(CleanUp(polymer)) ;
+                if(polymer.Count < lowestValue) lowestValue = polymer.Count;
+            }
+            Console.WriteLine(lowestValue);
+        }
+
+        static bool CleanUp(LinkedList<char> input)
+        {
+            bool changed = false;
+            LinkedListNode<char> lastNode = input.First;
+            while(lastNode != null && lastNode.Next != null)
+            {
+                if(ShouldRemove(lastNode.Value, lastNode.Next.Value))
+                {
+                    changed = true;
+                    var newLastNode = lastNode.Next.Next;
+                    input.Remove(lastNode.Next);
+                    input.Remove(lastNode);
+                    lastNode = newLastNode;
+                }
+                else
+                {
+                    lastNode = lastNode.Next;
+                }
+            }
+
+            return changed;
+        }
+
+        static bool ShouldRemove(char current, char next)
+        {
+            return (char.IsUpper(current) && char.IsLower(next) && current == char.ToUpper(next))
+                || (char.IsUpper(next) && char.IsLower(current) && next == char.ToUpper(current));
+        }
+    }
+}
