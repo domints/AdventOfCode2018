6d2b4dd9fda70d8a8233b0249d10eddf130deb4c
Dominik Szymański <domints@gmail.com>
2019-12-06 23:06:01 +0100

    Add day 7 stub

--- Day7/Part1/Program.cs
+++ Day7/Part1/Program.cs
@@ -9,11 +9,24 @@ namespace Day7
     {
         static void Main(string[] args)
         {
-            List<(int x, int y)> coords = File.ReadAllLines("input.txt")
+            var stepsRaw = File.ReadAllLines("input.txt")
                 .Select(l => l.Trim())
                 .Where(l => !string.IsNullOrWhiteSpace(l))
-                .Select(l => new { Step = l[36], Before = l[5]})
+                .Select(l => new { After = l[36], Before = l[5]})
                 .ToList();
+            Dictionary<char, Step> steps = new Dictionary<char, Step>();
+            foreach(var s in stepsRaw)
+            {
+                steps.TryAdd(s.Before, new Step { Id = s.Before });
+                steps.TryAdd(s.After, new Step { Id = s.After });
+            }
         }
+    }
+
+    class Step
+    {
+        public char Id { get; set; }
+        public List<Step> Before { get; set; }
+        public List<Step> After { get; set; }
     }
 }
